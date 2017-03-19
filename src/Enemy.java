2d12c9c85bb5d50cf810a3001c521659d5a59753
levifussell <levifussell@hotmail.com>
2017-03-19 00:21:07 +0000

    backend world simulation layout created with player scope

---
+++ src/Enemy.java
@@ -0,0 +1,22 @@
+/**
+ * Created by levi on 18/03/17.
+ */
+public class Enemy extends GameObject{
+
+  public Enemy(int posX, int posY)
+  {
+    super(posX, posY);
+  }
+
+  @Override
+  public void Update()
+  {
+
+  }
+
+  @Override
+  public void Draw()
+  {
+
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        GridWorld.LOAD();
+        GridWorld.LOAD_LEVEL("....H....");
+        GridWorld.DEBUG_DRAW();
+    }
+}
